da407a3c1c5bcda9787c4b7d35a81d0d063018fa
Isichei Chukwuka <chuks89@live.com>
2023-06-22 17:14:01 +0100

    added solution to exercise3

---
+++ Exercise3/Program.cs
@@ -0,0 +1,18 @@
+﻿using Exercise3;
+
+Student[] students = new Student[10];
+int[] marks = { 20, 45, 86, 68, 62, 71, 91, 78, 59, 63 };
+for (int i = 0; i < 10; i++)
+{
+    students[i] = new Student
+    {
+        Name = $"student{i + 1}",
+        Mark = marks[i],
+    };
+}
+
+Array.Sort(students);
+foreach (Student student in students)
+{
+    Console.WriteLine($"{student.Name} : {student.Mark}");
+}
---
+++ Exercise3/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3
+{
+    public class Student : IComparable<Student>
+    {
+        public string Name { get; set; }
+        public int Mark { get; set; }
+        public int CompareTo(Student other)
+        {
+            return Mark.CompareTo(other.Mark);
+        }
+    }
+}
